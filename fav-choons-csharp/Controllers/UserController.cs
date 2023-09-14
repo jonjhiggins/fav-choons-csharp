14dfb80f607aba3a52d884a0b29d41bdbc6e5191
Jon Higgins <jon@jonhiggins.co.uk>
2023-09-14 14:54:13 +0100

    Switch to MVC

---
+++ fav-choons-csharp/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace fav_choons_csharp.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: /user/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /user/profile/{id}
+        public IActionResult Profile(int id)
+        {
+            ViewData["Id"] = id.ToString();
+
+            return View();
+        }
+    }
+}
+
--- fav-choons-csharp/Program.cs
+++ fav-choons-csharp/Program.cs
@@ -7,15 +7,15 @@ ConfigurationManager configurationManager = builder.Configuration;
 
 
 // Add services to the container.
-builder.Services.AddRazorPages();
 builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -27,7 +27,8 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
-
